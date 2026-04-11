1cd753d489924a5e7ece95685e213228dcb0af8f
gummyhz <gummyrebecca@gmail.com>
2026-04-11 17:55:57 -0400

    update website

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // javascript file
+function filterImages(className) {
+  const allImages = document.querySelectorAll('.all-imgs');
+  allImages.forEach(img => img.style.display = 'none');
+
+  const selectedImages = document.querySelectorAll('.' + className);
+  selectedImages.forEach(img => img.style.display = 'block');
+}
+
+function openLightbox(src) {
+    const overlay = document.getElementById('dynamic-lightbox');
+    const fullImg = document.getElementById('lightbox-img');
+    
+    fullImg.src = src; 
+    overlay.style.display = 'flex'; 
+}
+
+function closeLightbox() {
+    document.getElementById('dynamic-lightbox').style.display = 'none';
+}
